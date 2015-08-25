bb9d82cae31b9bcd5cd3ec02b666dec98949afdb
madumoulin <maxence.dumoulin@viacesi.fr>
2015-08-25 14:18:35 +0200

    newskill

--- Scripts/semi-donut.js
+++ Scripts/semi-donut.js
@@ -1,132 +1,37 @@
-$(function() {
-    Highcharts.setOptions({
-        chart: {
-            backgroundColor: null,
-            borderWidth: 0,
-            plotBackgroundColor: null,
-            plotShadow: false,
-            plotBorderWidth: 0
-        },
-        colors:['#34495e','#c0392b','#bdc3c7'],
-        style:{
-               fontSize: '1vw',
-               border:'0',
-               width:'20vw',
-               height:'20vh'
-            },
-    });
-});
-$(document).ready(function (){
-    var dotNet= new Highcharts.Chart({
-        chart: {
-         renderTo: 'dotNet'
-        },
-        title: {
-            text: '.Net<br/> technologies',
-            align: 'center',
-            style:{
-               fontSize: '3vw',
-               padding:'0'
-            },
-            verticalAlign: 'middle',
+function resize(){
+    google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+}
+$(document).ready(resize());
+function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Competences', 'Competences per cent'],
+          ['MVC',     45],
+          ['ASP', 40],
+          ['TFS', 30]
+        ]);
 
-        },
-        tooltip: {
-            pointFormat: '{series.name}',
-             style: {
-                        fontSize: '1vw',
-                        width:'20vh',
-                        height:'20vh'
-                    }
-        },
-        plotOptions: {
-            pie: {
-                size:'80%',
-                dataLabels: {
-                    enabled: true,
-                    distance: -50,
-                    style: {
-                        fontSize: '1vw',
-                        color: 'transparent'
-                    }
-                },
-                startAngle: -90,
-                endAngle: 70,
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: '.Net',
-             style:{
-               fontSize: '1vw',
-               border:'1vw'
-            },
-            innerSize: '50%',
-            data: [
-                ['MVC',       45.00],
-                ['ASP',           35.00],
-            ],
-        }],
-        credits: {
-      enabled: false
-  }
-    });
+        var options = {
+          title: '.Net',
+          pieHole: 0.4,
+          backgroundColor:'none',
+          legend:'none',
+          width:'50%',
+          height:'80%',
+          maxZoomOut:1,
+        keepInBounds: true
     
-    var web= new Highcharts.Chart({
-        chart: {
-         renderTo: 'web'
-        },
-        title: {
-            text: 'Web<br/> technologies',
-            align: 'center',
-            style:{
-               fontSize: '3vw',
-               padding:'0'
-            },
-            verticalAlign: 'middle',
+        };
+         var chart = new google.visualization.PieChart(document.getElementById('dotNet'));
+        chart.draw(data, options);
+        
+}
+window.onresize= resize;
+var handler = window.onresize;
 
-        },
-        tooltip: {
-            pointFormat: '{series.name}',
-             style: {
-                        fontSize: '1vw',
-                        width:'20vh',
-                        height:'20vh'
-                    }
-        },
-        plotOptions: {
-            pie: {
-                size:'80%',
-                dataLabels: {
-                    enabled: true,
-                    distance: -50,
-                    style: {
-                        fontSize: '1vw',
-                        color: 'transparent'
-                    }
-                },
-                startAngle: -90,
-                endAngle: 88.33,
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: '.Net',
-             style:{
-               fontSize: '1vw',
-               border:'1vw'
-            },
-            innerSize: '50%',
-            data: [
-                ['Html5',       33.33],
-                ['CSS/LESS',           30.00],
-                ['JavaScript',           25.00]
-            ],
-        }],
-        credits: {
-      enabled: false
-  }
-    });
-});
+handler.apply(window, [' On']);
 
 
+       
+      
+      
